79f667b70a8bb03a97fd2bba35ed341eac811dd1
Jose Garcia <josebalius@github.com>
2021-10-26 11:08:10 -0400

    plug updates

--- install.sh
+++ install.sh
@@ -4,9 +4,14 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
+sudo apt-get --assume-yes install silversearcher-ag bat fuse
+
 # install latest neovim
+sudo modprobe fuse
+sudo groupadd fuse
+sudo usermod -a -G fuse "$(whoami)"
 wget https://github.com/neovim/neovim/releases/download/v0.5.1/nvim.appimage
-chmod u+x nvim.appimage
+sudo chmod u+x nvim.appimage
 mv nvim.appimage /usr/local/bin/nvim
 
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
@@ -18,9 +23,13 @@ rm -f $HOME/.zshrc
 ln -s $(pwd)/zshrc $HOME/.zshrc
 ln -s $(pwd)/bash_profile $HOME/.bash_profile
 
+rm -rf $HOME/.config
+mkdir $HOME/.config
+ln -s "$(pwd)/config/nvim" "$HOME/.config/nvim"
+
 nvim +'PlugInstall --sync' +qa
 
-sudo apt-get --assume-yes install silversearcher-ag bat
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
 
 sudo chsh -s "$(which zsh)" "$(whoami)"
+
