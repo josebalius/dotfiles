ffe7b9a3630af95fd22edcee0f0ce967aeec4539
Jose Garcia <josebalius@github.com>
2021-10-26 11:11:43 -0400

    update install

--- install.sh
+++ install.sh
@@ -4,7 +4,21 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-sudo apt-get --assume-yes install silversearcher-ag bat fuse
+# Install curl, tar, git, other dependencies if missing
+PACKAGES_NEEDED="\
+    silversearcher-ag \
+    bat \
+    fuse \
+    libfuse2"
+
+if ! dpkg -s ${PACKAGES_NEEDED} > /dev/null 2>&1; then
+    if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
+        sudo apt-get update
+    fi
+    sudo apt-get -y -q install ${PACKAGES_NEEDED}
+fi
+
+# sudo apt-get --assume-yes install silversearcher-ag bat fuse
 
 # install latest neovim
 sudo modprobe fuse
