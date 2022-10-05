24a0bb7811779b42ce9cea08db408ac421e1c732
Jose Garcia <josebalius@github.com>
2022-10-05 09:38:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,8 +28,9 @@ sudo modprobe fuse
 sudo groupadd fuse
 sudo usermod -a -G fuse "$(whoami)"
 # wget https://github.com/neovim/neovim/releases/download/v0.5.1/nvim.appimage
-wget https://github.com/github/copilot.vim/releases/download/neovim-nightlies/appimage.zip
-unzip appimage.zip
+#wget https://github.com/github/copilot.vim/releases/download/neovim-nightlies/appimage.zip
+#unzip appimage.zip
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 sudo chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 
