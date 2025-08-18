58fef2ce1d22bd5ddd27d6b9e06ed62a7c74af26
Jose Garcia <josebalius@github.com>
2025-08-18 14:39:50 -0400

    Update install.sh to set up environment and dependencies
    Install necessary packages and configure user environment.

--- install.sh
+++ install.sh
@@ -1,1 +1,52 @@
-Actions Development Codespace
+#!/bin/bash
+
+exec > >(tee -i $HOME/dotfiles_install.log)
+exec 2>&1
+set -x
+
+# Install curl, tar, git, other dependencies if missing
+PACKAGES_NEEDED="\
+    silversearcher-ag \
+    bat \
+    fuse \
+    dialog \
+    apt-utils \
+    libfuse2"
+
+if ! dpkg -s ${PACKAGES_NEEDED} > /dev/null 2>&1; then
+    if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
+        sudo apt-get update
+    fi
+    sudo echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+    sudo apt-get -y -q install ${PACKAGES_NEEDED}
+fi
+
+# sudo apt-get --assume-yes install silversearcher-ag bat fuse
+
+# install latest neovim
+sudo modprobe fuse
+sudo groupadd fuse
+sudo usermod -a -G fuse "$(whoami)"
+# wget https://github.com/neovim/neovim/releases/download/v0.5.1/nvim.appimage
+#wget https://github.com/github/copilot.vim/releases/download/neovim-nightlies/appimage.zip
+#unzip appimage.zip
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+sudo chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
+
+ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/vimrc $HOME/.vimrc
+ln -s $(pwd)/vim $HOME/.vim
+ln -s $(pwd)/emacs $HOME/.emacs
+ln -s $(pwd)/screenrc $HOME/.screenrc
+rm -f $HOME/.zshrc
+ln -s $(pwd)/zshrc $HOME/.zshrc
+ln -s $(pwd)/bash_profile $HOME/.bash_profile
+
+rm -rf $HOME/.config
+mkdir $HOME/.config
+ln -s "$(pwd)/config/nvim" "$HOME/.config/nvim"
+
+nvim +'PlugInstall --sync' +qa
+
+sudo chsh -s "$(which zsh)" "$(whoami)"
